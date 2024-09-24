111849c44b23458b02be997e7dd47d53e8b2b615
Ellie <ellierparry@gmail.com>
2024-09-24 13:55:49 +0100

    fetch data from api.

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,21 @@
+'use client'
+
 export default function Page() {
+  
+  async function getData() {
+    const data = await fetch("https://final-project-additional-repo-the.onrender.com/api/user");
+
+    console.log(data.json())
+  
+  }
+
+  getData();
+  
+
   return (
     <div>
+
+
     </div>
   )
 }
