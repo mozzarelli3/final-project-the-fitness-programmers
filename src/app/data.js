da85f4a2fb733a90892ee2d471cccd8ee7eb64ac
Isuu1 <jakub.babelek@gmail.com>
2024-09-25 11:05:20 +0100

    Added basic layout for homepage

---
+++ src/app/data.js
@@ -0,0 +1,17 @@
+export const user = [
+  {
+    id: 1,
+    name: "John Doe",
+    age: 25,
+    email: "john@test.com",
+    startWeight: 80,
+    currentWeight: 70,
+    targetWeight: 65,
+    height: 170,
+    bmi: 24.2,
+    targetCalories: 2000,
+    achievedCalories: 1000,
+    goals: [],
+    workouts: [],
+  },
+];
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,46 @@
-'use client'
+"use client";
+import { user } from "./data.js";
 
 export default function Page() {
-
-  async function getData() {
-    const data = await fetch("https://final-project-additional-repo-the.onrender.com/api/user");
-
-    console.log(data.json())
-
-  }
-
-  getData();
-
+  console.log(user);
 
   return (
     <div>
-      <h1>TESTESTETSTETSTETSTET</h1>
-
+      <h1>Progress</h1>
+      <div>
+        <div>
+          <p>Start weight</p>
+          <p>{user[0].startWeight}</p>
+        </div>
+        <div>
+          <p>Current weight</p>
+          <p>{user[0].currentWeight}</p>
+        </div>
+        <div>
+          <p>Target weight</p>
+          <p>{user[0].targetWeight}</p>
+        </div>
+      </div>
+      <div>
+        <h1>Chart here</h1>
+      </div>
+      <h1>Daily</h1>
+      <div>
+        <p>Target calories</p>
+        <p>{user[0].targetCalories}</p>
+      </div>
+      <div>
+        <p>Achieved calories</p>
+        <p>{user[0].achievedCalories}</p>
+      </div>
+      <div>
+        <p>Workout time / goal</p>
+        <p>1hour</p>
+      </div>
+      <div>
+        <p>Average workout time</p>
+        <p>30mins</p>
+      </div>
     </div>
-  )
+  );
 }
